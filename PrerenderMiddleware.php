fcd2f56c56df1a41fc27abac3ea3d4fa29851847
Mahonri Gibson <mahonrig@gmail.com>
2014-04-20 22:31:28 -0700

    Refactor for better logic, added ignored extensions.

--- PrerenderMiddleware.php
+++ PrerenderMiddleware.php
@@ -3,54 +3,95 @@ class PrerenderMiddleware extends \Slim\Middleware
 {
   protected $backendURL;
   protected $token;
-  protected $render = false;
 
-  public function __construct($backendURL, $token)
-  {
+  public function __construct($backendURL, $token){
       $this->backendURL = $backendURL;
       $this->token = $token;
   }
-  public function call()
-  {
-      //The Slim application
-      $app = $this->app;
 
-      //The Environment object
-      $env = $app->environment;
+  public function isBot(){
 
-      //The Request object
-      $req = $app->request;
+    // Google and other engines using this
+    if (isset($_GET['_escaped_fragment_'])){
+      return true;
+    }
 
-      //The Response object
-      $res = $app->response;
+    $agent = $this->app->request->getUserAgent();
+    // regex with our bot list
+    $bots = "!(Googlebot|bingbot|Googlebot-Mobile|Yahoo|YahooSeeker|FacebookExternalHit|Twitterbot|TweetmemeBot|BingPreview|developers.google.com/\+/web/snippet/)!i";
 
-      $agent = $req->getUserAgent();
-      $bots = "!(Googlebot|bingbot|Googlebot-Mobile|Yahoo|YahooSeeker|FacebookExternalHit|Twitterbot|TweetmemeBot|BingPreview|developers.google.com/\+/web/snippet/)!i";
+    // if anything in our search string is in the user agent
+    if (preg_match($bots, $agent)){
+      return true;
+    }
 
-      if (isset($_GET['_escaped_fragment_'])){
-        $init = $this->backendURL . $env['slim.url_scheme'] . '://' . $env['HTTP_HOST'] . '/?_escaped_fragment_=' . $_GET['_escaped_fragment_'];
-        $this->render = true;
-      } else if(preg_match($bots, $agent)){
-        $resourceUri = $req->getResourceUri();
-        $init = $this->backendURL . $env['slim.url_scheme'] . '://' . $env['HTTP_HOST'] . $resourceUri;
-        $this->render = true;
-      }
+    // not a bot
+    return false;
+  }
 
-      if ($this->render){
-        $ch = curl_init($init);
-        $xtoken = 'X-Prerender-Token: ' . $this->token;
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array($xtoken));
-        curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-        curl_setopt($ch, CURLOPT_HEADER, 0);            // No header in the result
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return, do not echo result
+  public function isIgnoredExtension(){
+    $resourceURI = $this->app->request->getResourceUri();
+    $extensions = "!(\.js|\.css|\.xml|\.less|\.png|\.jpg|\.jpeg|\.gif|\.pdf|\.doc|\.txt|\.ico|\.rss|\.zip|\.mp3|\.rar|\.exe|\.wmv|\.doc|\.avi|\.ppt|\.mpg|\.mpeg|\.tif|\.wav|\.mov|\.psd|\.ai|\.xls|\.mp4|\.m4a|\.swf|\.dat|\.dmg|\.iso|\.flv|\.m4v|\.torrent)!i";
+    if (preg_match($extensions, $resourceURI)){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-        /* Fetch and return content, save it. */
-        $prerender = curl_exec($ch);
-        curl_close($ch);
-        $app->response->setBody($prerender);
+  public function shouldPreRender(){
+    // return false if not a bot
+    if (!$this->isBot()){
+      return false;
+    }
 
-      } else { /* Not coming from a bot, render as usual */
-          $this->next->call();
+    // don't preRender if ignored extension
+    if ($this->isIgnoredExtension()){
+      return false;
+    }
+
+    return true;
+  }
+
+  public function preRender(){
+
+    $env = $this->app->environment;
+    $resourceURI = $this->app->request->getResourceUri();
+    $agent = $this->app->request->getUserAgent();
+
+    // Our request string
+    $url = $this->backendURL . $env['slim.url_scheme'] . '://' . $env['HTTP_HOST'] . $resourceURI;
+
+    // Our token
+    $xtoken = 'X-Prerender-Token: ' . $this->token;
+
+    // Init cURL
+    $ch = curl_init($url);
+
+    // Send our token
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array($xtoken));
+
+    // Set user agent
+    curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+
+    // Don't need header
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+
+    // Hold onto return
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return, do not echo result
+
+    // Fetch and return content, set into body
+    $prerender = curl_exec($ch);
+    curl_close($ch);
+    $this->app->response->setBody($prerender);
+  }
+
+  public function call(){
+
+      if ($this->shouldPreRender()){
+        $this->preRender();
+      } else {
+        $this->next->call(); // render as usual
       }
   }
 }
